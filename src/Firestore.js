63a4bcb1691e68b835fb619b3bd1aeb0c2e497cb
Amelie Oller <amelieoller@gmail.com>
2019-03-18 19:45:50 -0700

    Create firestore

---
+++ src/Firestore.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: 'amelie-notes',
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+};
+
+firebase.initializeApp(config);
+const db = firebase.firestore();
+
+export default db;
