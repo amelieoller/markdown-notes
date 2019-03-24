0bc6e3e2b846d280d14fe92ee7819f8636698baf
Amelie Oller <amelieoller@gmail.com>
2019-03-24 15:30:47 -0700

    Create tags component

---
+++ src/components/Tags.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tags = () => (
+  <div className="tags">
+    <div className="tag">JavaScript</div>
+    <div className="tag">Ruby</div>
+    <div className="tag">Bash</div>
+  </div>
+);
+
+export default Tags;
