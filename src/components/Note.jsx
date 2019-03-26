d6c277effe477bcd0469ba0d2b38aa2e1b21e49c
Amelie Oller <amelieoller@gmail.com>
2019-03-25 20:10:46 -0700

    Add redux/firestore to note/notes components

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,32 +1,60 @@
 import React from 'react';
-import Markdown from 'react-markdown/with-html';
 import PropTypes from 'prop-types';
+import Markdown from 'markdown-to-jsx';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 import CodeBlock from './CodeBlock';
-import db from '../Firestore';
+import { deleteNote } from '../actions/noteActions';
 
-const handleDeleteNote = (key) => {
-  db.collection('notes')
-    .doc(key)
-    .delete();
-};
-
-const Note = ({ note }) => (
+const Note = ({
+  note, deleteNote, setEditNote, tags,
+}) => (
   <div className="note">
     <Markdown
-      className="result"
-      source={note.content}
-      renderers={{ code: CodeBlock }}
-    />
-    <div>
-      <button
-        type="submit"
-        onClick={() => {
-          handleDeleteNote(note.id);
-        }}
-        className="delete-button"
-      >
-            Delete
-      </button>
+      options={{
+        forceBlock: true,
+        overrides: {
+          code: {
+            component: CodeBlock,
+          },
+        },
+      }}
+    >
+      {note.content}
+    </Markdown>
+    <div className="footer">
+      <div className="left">
+        <button
+          type="submit"
+          onClick={() => {
+            setEditNote(note);
+          }}
+          className="edit-button"
+        >
+          Edit
+        </button>
+        <button
+          type="submit"
+          onClick={() => {
+            const result = window.confirm('Want to delete?');
+            result && deleteNote(note.id);
+          }}
+          className="delete-button"
+        >
+          Delete
+        </button>
+      </div>
+      <div className="right tags">
+        {note.tagIds
+          && tags
+            .filter(t => note.tagIds.includes(t.id))
+            .map(tag => (
+              <div key={tag.id} className="tag">
+                {tag.name}
+              </div>
+            ))}
+      </div>
     </div>
   </div>
 );
@@ -34,8 +62,25 @@ const Note = ({ note }) => (
 Note.propTypes = {
   note: PropTypes.shape({
     content: PropTypes.string,
-    created: PropTypes.instanceOf(Date),
+    created: PropTypes.object,
   }).isRequired,
+  setEditNote: PropTypes.func.isRequired,
+  deleteNote: PropTypes.func.isRequired,
+  tags: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default Note;
+const mapDispatchToProps = dispatch => ({
+  deleteNote: key => dispatch(deleteNote(key)),
+});
+
+const mapStateToProps = state => ({
+  tags: state.firestore.ordered.tags,
+});
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+  firestoreConnect([{ collection: 'tags' }]),
+)(Note);
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,39 +1,34 @@
-import React, { Component } from 'react';
-import db from '../Firestore';
+import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import PropTypes from 'prop-types';
 import Note from './Note';
 
-class Notes extends Component {
-  constructor() {
-    super();
+const Notes = ({ notes, setEditNote, tagFilter }) => (
+  <div className="notes">
+    {notes
+      && notes
+        .filter(note => tagFilter.every(tagId => note.tagIds.includes(tagId)))
+        .sort(
+          (a, b) => (b.updated ? b.updated.toDate() : new Date())
+            - (a.updated ? a.updated.toDate() : new Date()),
+        )
+        .map(note => <Note key={note.id} note={note} setEditNote={setEditNote} />)}
+  </div>
+);
 
-    this.state = {
-      notes: [],
-    };
-  }
+Notes.propTypes = {
+  notes: PropTypes.arrayOf(PropTypes.object),
+  setEditNote: PropTypes.func.isRequired,
+  tagFilter: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
-  componentDidMount = () => {
-    const newNotes = [];
-    db.collection('notes')
-      .get()
-      .then((querySnapshot) => {
-        querySnapshot.forEach((doc) => {
-          newNotes.push({ id: doc.id, ...{ ...doc.data() } });
-        });
-        this.setState({
-          notes: newNotes,
-        });
-      });
-  };
+const mapStateToProps = state => ({
+  notes: state.firestore.ordered.notes,
+});
 
-  render() {
-    const { notes } = this.state;
-
-    return (
-      <div className="notes">
-        {notes && notes.map(note => <Note key={note.id} note={note} />)}
-      </div>
-    );
-  }
-}
-
-export default Notes;
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'notes' }]),
+)(Notes);
