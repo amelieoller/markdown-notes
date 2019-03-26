b9f703554e143c167ddf95cbb5eb34660e89854e
Amelie Oller <amelieoller@gmail.com>
2019-03-25 20:11:30 -0700

    Add redux to tags component

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
+import CreateTag from './CreateTag';
+import { deleteTag } from '../actions/tagActions';
 
-const Tags = () => (
+const Tags = ({
+  tags, setTagFilter, filteredTags, deleteTag,
+}) => (
   <div className="tags">
-    <div className="tag">JavaScript</div>
-    <div className="tag">Ruby</div>
-    <div className="tag">Bash</div>
+    {tags
+      && tags.map(tag => (
+        <div key={tag.id} className={filteredTags.includes(tag.id) ? 'tag' : 'greyTag'}>
+          <span key={tag.id} onClick={() => setTagFilter(tag.id)}>
+            {tag.name}
+          </span>
+          <span
+            onClick={() => {
+              const result = window.confirm('Want to delete?');
+              result && deleteTag(tag.id);
+            }}
+          >
+            {' '}
+            | x
+          </span>
+        </div>
+      ))}
+    <CreateTag />
   </div>
 );
 
-export default Tags;
+const mapStateToProps = state => ({
+  tags: state.firestore.ordered.tags,
+});
+
+const mapDispatchToProps = dispatch => ({
+  deleteTag: key => dispatch(deleteTag(key)),
+});
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+  firestoreConnect([{ collection: 'tags' }]),
+)(Tags);
