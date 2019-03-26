d8657fef80d72023b55232ac7f87b15c25cf9190
Amelie Oller <amelieoller@gmail.com>
2019-03-25 20:04:58 -0700

    Update code block component with syntax highlight

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,53 +1,21 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { tomorrowNightEighties } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import PropTypes from 'prop-types';
 
-class CodeBlock extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.setRef = this.setRef.bind(this);
-  }
-
-  componentDidMount() {
-    this.highlightCode();
-  }
-
-  componentDidUpdate() {
-    this.highlightCode();
-  }
-
-  setRef(el) {
-    this.codeEl = el;
-  }
-
-  highlightCode() {
-    return (
-      <SyntaxHighlighter language="javascript" style={dark}>
-        {this.codeEl}
-      </SyntaxHighlighter>
-    );
-  }
-
-  render() {
-    const { value, language } = this.props;
-    return (
-      <pre>
-        <code ref={this.setRef} className={`language-${language}`}>
-          {value}
-        </code>
-      </pre>
-    );
-  }
-}
-
-CodeBlock.defaultProps = {
-  language: '',
-};
+const CodeBlock = ({ children }) => (
+  <SyntaxHighlighter
+    className="code-block"
+    language="javascript"
+    style={tomorrowNightEighties}
+    showLineNumbers
+  >
+    {children}
+  </SyntaxHighlighter>
+);
 
 CodeBlock.propTypes = {
-  value: PropTypes.string.isRequired,
-  language: PropTypes.string,
+  children: PropTypes.string.isRequired,
 };
 
 export default CodeBlock;
