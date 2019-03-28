bdc59220a20223ccd77276491ecdbf817cd5a778
Amelie Oller <amelieoller@gmail.com>
2019-03-28 07:15:36 -0700

    Create icon component and icon constants

---
+++ src/components/Icon.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({ color, size, icon }) => {
+  const styles = {
+    svg: {
+      display: 'inline-block',
+      verticalAlign: 'middle',
+    },
+    path: {
+      fill: color,
+    },
+  };
+
+  return (
+    <svg style={styles.svg} width={`${size}px`} height={`${size}px`} viewBox="0 0 1024 1024">
+      <path style={styles.path} d={icon} />
+    </svg>
+  );
+};
+
+Icon.propTypes = {
+  icon: PropTypes.string.isRequired,
+  size: PropTypes.number,
+  color: PropTypes.string,
+};
+
+Icon.defaultProps = {
+  size: 16,
+  color: 'rgb(45, 45, 45)',
+};
+
+export default Icon;
