ec6f26a20b510fcddf92cd1047c91ac08b86cad4
Amelie Oller <amelieoller@gmail.com>
2019-03-31 10:19:45 -0700

    Make tags styled component

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -2,13 +2,35 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
+import styled from 'styled-components';
 import CreateTag from './CreateTag';
 import { deleteTag } from '../actions/tagActions';
+
+const StyledTags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  color: white;
+
+  .tag {
+    margin: 0.2em 0.3em;
+    font-size: 0.9em;
+    font-weight: 300;
+    cursor: pointer;
+    text-decoration: underline;
+  }
+
+  .greyTag {
+    margin: 0.2em 0.3em;
+    font-size: 0.9em;
+    font-weight: 300;
+    cursor: pointer;
+  }
+`;
 
 const Tags = ({
   tags, setTagFilter, filteredTags, deleteTag,
 }) => (
-  <div className="tags">
+  <StyledTags>
     {tags
       && tags.map(tag => (
         <div key={tag.id} className={filteredTags.includes(tag.id) ? 'tag' : 'greyTag'}>
@@ -27,7 +49,7 @@ const Tags = ({
         </div>
       ))}
     <CreateTag />
-  </div>
+  </StyledTags>
 );
 
 const mapStateToProps = state => ({
