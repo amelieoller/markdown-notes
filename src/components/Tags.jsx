68a21a848b072d4f02b540f2dd3c3d49a4755a0e
Amelie Oller <amelieoller@gmail.com>
2019-04-06 16:17:31 -0700

    Style filter tags

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -6,50 +6,65 @@ import styled from 'styled-components';
 import CreateTag from './CreateTag';
 import { deleteTag } from '../actions/tagActions';
 
-const StyledTags = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+const StyledTag = styled.span`
   color: white;
 
   .tag {
-    margin: 0.2em 0.3em;
-    font-size: 0.9em;
+    margin: 0em 0.5em 0.5em 0em;
+    font-size: 0.8em;
     font-weight: 300;
     cursor: pointer;
-    text-decoration: underline;
+    border: 1px solid grey;
+    padding: 4px 12px;
+    border-radius: 15px;
+    border-color: ${props => props.theme.lightPrimaryHighlight};
+
+    &:hover {
+      transition: background-color 0.2s ease;
+      background-color: ${props => props.theme.lightPrimaryHighlight};
+    }
   }
 
-  .greyTag {
-    margin: 0.2em 0.3em;
-    font-size: 0.9em;
-    font-weight: 300;
-    cursor: pointer;
+  .highlighted {
+    background: ${props => props.theme.lightPrimaryHighlight};
+  }
+
+  .delete {
+    color: ${props => props.theme.lightPrimaryHighlight};
+    margin-left: 8px;
+
+    &:hover {
+      transition: color 0.2s ease;
+      color: white;
+    }
   }
 `;
 
 const Tags = ({
   tags, setTagFilter, filteredTags, deleteTag,
 }) => (
-  <StyledTags>
+  <>
     {tags
       && tags.map(tag => (
-        <div key={tag.id} className={filteredTags.includes(tag.id) ? 'tag' : 'greyTag'}>
-          <span key={tag.id} onClick={() => setTagFilter(tag.id)}>
-            {tag.name}
-          </span>
-          <span
-            onClick={() => {
-              const result = window.confirm('Want to delete?');
-              result && deleteTag(tag.id);
-            }}
-          >
-            {' '}
-            x
-          </span>
-        </div>
+        <StyledTag>
+          <div key={tag.id} className={filteredTags.includes(tag.id) ? 'tag highlighted' : 'tag'}>
+            <span key={tag.id} onClick={() => setTagFilter(tag.id)}>
+              {tag.name}
+            </span>
+            <span
+              onClick={() => {
+                const result = window.confirm('Want to delete?');
+                result && deleteTag(tag.id);
+              }}
+              className="delete"
+            >
+              x
+            </span>
+          </div>
+        </StyledTag>
       ))}
     <CreateTag />
-  </StyledTags>
+  </>
 );
 
 const mapStateToProps = state => ({
