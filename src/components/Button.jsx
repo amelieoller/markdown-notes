feca6c1a005035b0f46d0f5023ee3c18d52dad1b
Amelie Oller <amelieoller@gmail.com>
2019-04-06 16:11:39 -0700

    Add proptypes to button and modify styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledButton = styled.button`
   background: white;
   color: ${props => props.theme.primaryHighlight};
-  font-size: 1em;
+  font-size: ${props => (props.small ? '.8em' : '1em')};
   margin: 0.5em;
-  padding: 0.25em 1em;
+  padding: ${props => (props.small ? '0.1em .4em' : '0.25em 1em')};
   border: 2px solid ${props => props.theme.primaryHighlight};
   border-radius: 3px;
   cursor: pointer;
 
   &:hover,
   &:active {
-    background: ${props => props.theme.primaryHighlight};
+    transition: all 0.3s ease;
+    background-color: ${props => props.theme.primaryHighlight};
     color: white;
   }
 `;
 
-const Button = ({ text, onClick, primary }) => (
-  <StyledButton type="submit" onClick={() => onClick()}>
-    {text}
-  </StyledButton>
-);
+const Button = (props) => {
+  const { text, onClick } = props;
+  return (
+    <StyledButton type="submit" onClick={() => onClick()} {...props}>
+      {text}
+    </StyledButton>
+  );
+};
+
+Button.defaultProps = {
+  text: 'Submit',
+};
+
+Button.propTypes = {
+  text: PropTypes.string,
+};
 
 export default Button;
