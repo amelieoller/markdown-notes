84265a3f941e9efece5f76b87dac9b14b18e0b35
Amelie Oller <amelieoller@gmail.com>
2020-07-12 21:59:28 -0700

    Update LinkNotes finding of notes and styles

--- src/components/LinkNotes.js
+++ src/components/LinkNotes.js
@@ -1,42 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/macro';
 
 import Search from './Search';
 import MarkdownFormattedText from './MarkdownFormattedText';
-import Button from './Button';
+import Button from '../atoms/Button';
 
-const LinkNotes = ({ addNoteIdLink }) => {
+const LinkNotes = ({ addNoteIdLink, linkIds, previousLinkedNotes }) => {
   const [foundNotes, setFoundNotes] = useState([]);
 
+  useEffect(() => {
+    setFoundNotes(previousLinkedNotes);
+  }, [previousLinkedNotes]);
+
   const getNoteIds = (notes) => {
-    setFoundNotes(notes);
+    const newFilteredNotes = notes.filter((note) => !linkIds.includes(note.id));
+    const prevFilteredNotes = foundNotes.filter((note) => linkIds.includes(note.id));
+    setFoundNotes([...prevFilteredNotes, ...newFilteredNotes]);
   };
 
   const addLink = (noteId) => {
     addNoteIdLink(noteId);
   };
 
+  const clearSearch = () => {
+    setFoundNotes((prevNotes) => prevNotes.filter((note) => linkIds.includes(note.id)));
+  };
+
   return (
     <StyledLinkNotes>
-      <Search setSearchResultNotes={getNoteIds} placeholderText="Link Notes" />
+      <Search
+        setSearchResultNotes={getNoteIds}
+        border
+        placeholderText="Link Notes"
+        clearSearch={clearSearch}
+      />
       {foundNotes &&
         foundNotes.map((note) => (
-          <Button key={note.id} onClick={() => addLink(note.id)} text={note.title} />
+          <Button
+            key={note.id}
+            onClick={() => addLink(note.id)}
+            isActive={linkIds && linkIds.includes(note.id)}
+            small
+            faded
+          >
+            {note.title}
+          </Button>
         ))}
     </StyledLinkNotes>
   );
 };
 
 const StyledLinkNotes = styled.div`
-  background: white;
-  min-height: 60px;
   display: flex;
   flex-wrap: wrap;
-  align-items: flex-start;
+
+  & > *:first-child {
+    max-width: 200px;
+  }
+
+  & > *:not(:last-child) {
+    margin-right: 5px;
+  }
 
   & > * {
-    margin-right: 5px;
+    margin-bottom: 5px;
   }
 
   input {
